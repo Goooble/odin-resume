2bfbd2a517d540206448d045287413ca840f9d91
Goooble <bivinbheemaiah@gmail.com>
2025-10-20 09:55:29 +0530

    feat: display function for Education

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
-import { useState } from 'react'
-import PIEdit from './PIEdit'
-import Resume from './Resume'
+import { useState } from "react";
+import PIEdit from "./PIEdit";
+import InputField from "./InputField";
+import Resume from "./Resume";
+import EducationEdit from "./EducationEdit";
 
 // import './App.css'
 
 function App() {
-  const [data, setData] = useState(
-    {
-      PI: {0:"", 1:"", 2:"", 3:""}
-    }
-  )
+  const [data, setData] = useState({
+    PI: { 0: "", 1: "", 2: "", 3: "" },
+    degree: {
+      0: { 0: "school", 1: "degree", 2: "start", 3: "end", 4: "location" },
+    },
+  });
   return (
-  <>
-  <PIEdit data={data} setData={setData}></PIEdit>
-  <Resume data={data}></Resume>
-  </>
-  )
+    <>
+      <div className="edit-container">
+        <PIEdit data={data} setData={setData}></PIEdit>
+
+      </div>
+      <Resume data={data}></Resume>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/Resume.jsx
+++ src/Resume.jsx
@@ -1,25 +1,31 @@
-function Resume({data}){
-    return (
-        <div className="resume">
-            <h2>Personal Information: </h2>
-            <div className="full-name">
-                <h3>Full Name:</h3>
-                <p>{data.PI[0]}</p>
-            </div>
-            <div className="email">
-                <h3>Email:</h3>
-                <p>{data.PI[1]}</p>
-            </div>
-            <div className="phone">
-                <h3>Phone Number:</h3>
-                <p>{data.PI[2]}</p>
-            </div>
-            <div className="address">
-                <h3>Address:</h3>
-                <p>{data.PI[3]}</p>
-            </div>
+import EducationBlock from "./EducationBlock";
+function Resume({ data }) {
+  return (
+    <div className="resume">
+      <div className="pi-class">
+        <h2>Personal Information: </h2>
+        <div className="full-name">
+          <h3>Full Name:</h3>
+          <p>{data.PI[0]}</p>
         </div>
-    )
+        <div className="email">
+          <h3>Email:</h3>
+          <p>{data.PI[1]}</p>
+        </div>
+        <div className="phone">
+          <h3>Phone Number:</h3>
+          <p>{data.PI[2]}</p>
+        </div>
+        <div className="address">
+          <h3>Address:</h3>
+          <p>{data.PI[3]}</p>
+        </div>
+        <div className="education-class">
+            <EducationBlock data={data} id={0}></EducationBlock>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Resume;
